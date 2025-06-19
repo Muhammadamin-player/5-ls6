64257703046204f608a177badaab3896725d9b45
Muhammadamin-player <ibrohimovmuhammadamin888@gmail.com>
2025-06-19 22:01:50 +0500

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,13 @@
+let nameFruit = confirm(' Mevalarni jamisi ' + [4])
+let nameFruit3 = [' Olma ' , ' Banan ' , ' Shaptoli ' , ' Orik ']
+let nameFruit2 = confirm(' sizdan bitta  meva olip tashaymiz')
+
+if(nameFruit2){
+    nameFruit3.pop(nameFruit3)
+}
+else{
+    nameFruit3v
+}
+
+
+console.log(nameFruit3);
